1b900474af0a2bc3a1820d4ab68d03cd6e46b22a
EC2 Default User <ec2-user@ip-172-31-89-85.ec2.internal>
2023-07-31 15:58:50 +0000

    Adding backend

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
+
 terraform {
+	backend "remote" {
+	organization = "ACG-Terraform-Course-Kreso"
+	workspaces {
+		name = "Example-Workspace"
+}
+}
   required_providers {
     aws = {
       source  = "hashicorp/aws"
